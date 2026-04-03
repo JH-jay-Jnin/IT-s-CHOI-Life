c6af9ca924fc48e97b94d4560f96d80d58cf3c04
JH-jay-Jnin <jjustice551@gmail.com>
2026-04-03 13:51:29 +0900

    오후 수업 끝.

--- todolist-app-router_before/src/App.vue
+++ todolist-app-router_before/src/App.vue
@@ -7,15 +7,36 @@
 <script setup>
 import { reactive, computed, provide } from 'vue';
 import Header from '@/components/Header.vue';
+import axios from 'axios';
 
 const states = reactive({
-  todoList: [
-    { id: 1, todo: 'ES6학습', desc: '설명1', done: false },
-    { id: 2, todo: 'React학습', desc: '설명2', done: false },
-    { id: 3, todo: 'ContextAPI 학습', desc: '설명3', done: true },
-    { id: 4, todo: '야구경기 관람', desc: '설명4', done: false },
-  ],
+  todoList: [],
 });
+
+const BASEURI = '/api/todos';
+
+//1. 컴포넌트 초기화될 때 json-server로 연결해서 전체 목록 가지고 와야함.
+//2.  백엔드 서버로 부터 가지고 온 목록을 todoList에 넣어 주어야함.
+//3. 화면에 todoList목록을 보일 것임.
+
+const fetchTodoList = async () => {
+  // axios 필요함. --> import 필요함.
+  try {
+    let response = await axios.get(BASEURI); //http통신 ( 요청을 보냄 ) --> http 응답을 받음
+    if (response.status == 200) {
+      console.log(response.data); // 콘솔에 찍어 넣어 주는 습관이 좋다.
+      //states.todoList에 데이터를 넣어주어야 화면에 나타남.
+      states.todoList = response.data; // response.data가 배열이기 때문에 따로 코드를 추가적으로 안쓰고 그대로 대입.
+    } else {
+      console.log('데이터 전체 조회 실패');
+    }
+  } catch (e) {
+    console.log('예상치 못한 에러가 발생함. 에러 정보는 ' + e);
+  }
+};
+
+fetchTodoList(); //시작하자 함수를 호출하고 싶으면 바로 써주기.
+
 const addTodo = ({ todo, desc }) => {
   states.todoList.push({ id: new Date().getTime(), todo, desc, done: false });
 };
