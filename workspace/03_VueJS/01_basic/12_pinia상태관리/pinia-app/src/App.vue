053ba9ce17351490776d83ec458f4f5de7df70d1
JH-jay-Jnin <jjustice551@gmail.com>
2026-04-06 16:41:23 +0900

    Pinia 상태 관리 예제 실습 - 4월 7일 오전에 이어서 할 에정 . (좋아요 5배 증가 까지 완료 )

---
+++ workspace/03_VueJS/01_basic/12_pinia상태관리/pinia-app/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Like from '@/components/Like.vue';
+
+// 01_basic
+import { useLikeStore } from '@/stores/01_basic/like';
+
+const likeStore = useLikeStore();
+</script>
+
+<template>
+  <div>
+    <h1>Pinia 상태 관리 예제</h1>
+    <h2>좋아요 수 : {{ likeStore.like }}</h2>
+    <h2>좋아요 수 5배 증가 : {{ likeStore.fivetimes }}</h2>
+  </div>
+  <Like />
+  <!-- 이 좋아요 버튼을 누를 때마다 증가했으면 좋겠다.-->
+</template>
+
+<style scoped></style>
---
+++ workspace/03_VueJS/01_basic/12_pinia상태관리/pinia-app/src/components/Like.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button @click="increase">좋아요 누르기</button>
+    <!-- 누를때마다 증가 -->
+  </div>
+</template>
+
+<script setup>
+// 01_basic
+import { useLikeStore } from '@/stores/01_basic/like';
+const { increase } = useLikeStore();
+// store 안에서 increase만 꺼내서 담기
+</script>
